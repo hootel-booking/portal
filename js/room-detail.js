42258114a5d2c4ae21af47536e84c373402764cb
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2023-10-24 02:07:24 +0700

    feature add room to cart

--- js/room-detail.js
+++ js/room-detail.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   // get id room
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const idRoom = urlParams.get("id");
+  const idRoom = Number(urlParams.get("id"));
+  const idUser = 1;
 
   $.ajax({
     url: `http://localhost:8080/rooms/id=${idRoom}`,
@@ -26,6 +27,7 @@ $(document).ready(function () {
                           <i class="icon_star-half_alt"></i>
                       </div>
                       <a href="#">Booking Now</a>
+                      <a href="#" class="add-to-cart-btn">Add To Cart</a>
                   </div>
               </div>
               <h2>159$<span>/Pernight</span></h2>
@@ -46,5 +48,33 @@ $(document).ready(function () {
       `;
 
     getTagIdRoom.innerHTML = htmlDisplay;
+  });
+
+  $(document).on("click", ".add-to-cart-btn", function () {
+    $.ajax({
+      url: `http://localhost:8080/carts`,
+      method: "post",
+      contentType: "application/json",
+      data: JSON.stringify({
+        idRoom: idRoom,
+        idUser: idUser,
+      }),
+    }).done(function (data) {
+      if (data.data) {
+        const getElIdCartCount = document.getElementById("lblCartCount");
+        let contentCartCount = $("#lblCartCount").text();
+        if (contentCartCount === "") {
+          htmlDisplay = 1 + "";
+          getElIdCartCount.innerHTML = htmlDisplay;
+        } else {
+          htmlDisplay = Number(contentCartCount) + 1 + "";
+          getElIdCartCount.innerHTML = htmlDisplay;
+        }
+
+        alert("Success");
+      } else {
+        alert("Error");
+      }
+    });
   });
 });
